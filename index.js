453e46138712c16cde30dbb979f0323af7369ee1
Anthony Campolo <12433465+ajcwebdev@users.noreply.github.com>
2024-03-30 15:05:49 -0500

    error fixed

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ import { Ragged } from "ragged"
 
 const { OPENAI_API_KEY } = process.env
 
-// const OPENAI_API_KEY = "x54TUTzuc1dRNx0m8HxjT3BlbkFJfkU9oMGjWLZrjovoyI0A"
-
-const r = new Ragged({
+const raggedClient = new Ragged({
   openai: { apiKey: OPENAI_API_KEY }
 })
 
-r.qPredict("What is Toronto?")
+raggedClient
+  .qPredict("What is Toronto?")
   .then(console.log)
   .catch(console.error)
-// Toronto is a city in Canada. It has a population of...
